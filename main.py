fc6945dcdce61ac34e67d113096e508485ea8fbe
vlod-bigiron <vlad.adochitei@protonmail.com>
2023-01-20 23:06:17 +0200

    Tidy up

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from pytube import YouTube
 
 def Download(link):
     youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
-    
+    video = youtubeObject.streams.get_highest_resolution()
+
     try:
-        youtubeObject.download()
+        video.download()
     except:
         print("Download failed, check internet connection")
         return 1
